517211b8a9d99cebb2b9ac90cc962bbe3863dbc3
Tiqur <trevorbrage02@gmail.com>
2025-01-15 23:40:13 -0800

    Color individual faces

--- src/main.cpp
+++ src/main.cpp
@@ -152,29 +152,55 @@ private:
 
 float vertices[] = {
     // Positions        // Colors (RGB)
-    0.5f,  0.5f,  -0.5f, 1.0f, 0.0f, 0.0f,
-    0.5f,  -0.5f, -0.5f, 1.0f, 1.0f, 0.0f,
-    -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
-    -0.5f, 0.5f,  -0.5f, 0.0f, 0.0f, 1.0f,
-    0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 1.0f,
-    0.5f,  -0.5f, 0.5f,  0.0f, 1.0f, 1.0f,
-    -0.5f, -0.5f, 0.5f,  1.0f, 1.0f, 1.0f,
-    -0.5f, 0.5f,  0.5f,  0.5f, 0.5f, 0.5f,
+    0.5f,  0.5f,  -0.5f, 1.0f, 0.0f, 0.0f, // Front face (Red)
+    -0.5f, 0.5f,  -0.5f, 1.0f, 0.0f, 0.0f, // Front face (Red)
+    -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f, // Front face (Red)
+    0.5f,  -0.5f, -0.5f, 1.0f, 0.0f, 0.0f, // Front face (Red)
+
+    0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 0.0f, // Back face (Green)
+    -0.5f, 0.5f,  0.5f,  0.0f, 1.0f, 0.0f, // Back face (Green)
+    -0.5f, -0.5f, 0.5f,  0.0f, 1.0f, 0.0f, // Back face (Green)
+    0.5f,  -0.5f, 0.5f,  0.0f, 1.0f, 0.0f, // Back face (Green)
+
+    0.5f,  0.5f,  0.5f,  0.0f, 0.0f, 1.0f, // Right face (Blue)
+    0.5f,  0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, // Right face (Blue)
+    0.5f,  -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, // Right face (Blue)
+    0.5f,  -0.5f, 0.5f,  0.0f, 0.0f, 1.0f, // Right face (Blue)
+
+    -0.5f, 0.5f,  0.5f,  1.0f, 1.0f, 0.0f, // Left face (Yellow)
+    -0.5f, 0.5f,  -0.5f, 1.0f, 1.0f, 0.0f, // Left face (Yellow)
+    -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 0.0f, // Left face (Yellow)
+    -0.5f, -0.5f, 0.5f,  1.0f, 1.0f, 0.0f, // Left face (Yellow)
+
+    0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 1.0f, // Top face (Cyan)
+    -0.5f, 0.5f,  0.5f,  0.0f, 1.0f, 1.0f, // Top face (Cyan)
+    -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f, 1.0f, // Top face (Cyan)
+    0.5f,  0.5f,  -0.5f, 0.0f, 1.0f, 1.0f, // Top face (Cyan)
+
+    0.5f,  -0.5f, 0.5f,  1.0f, 0.0f, 1.0f, // Bottom face (Magenta)
+    -0.5f, -0.5f, 0.5f,  1.0f, 0.0f, 1.0f, // Bottom face (Magenta)
+    -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 1.0f, // Bottom face (Magenta)
+    0.5f,  -0.5f, -0.5f, 1.0f, 0.0f, 1.0f, // Bottom face (Magenta)
 };
 
 unsigned int indices[] = {
-    3, 0, 1, //
-    3, 2, 1, //
-    2, 1, 5, //
-    2, 6, 5, //
-    1, 0, 4, //
-    1, 5, 4, //
-    3, 2, 6, //
-    3, 7, 6, //
-    6, 5, 4, //
-    7, 6, 4, //
-    7, 4, 0, //
-    7, 3, 0, //
+    0,  1,  2, // Front face
+    0,  2,  3, // Front face
+
+    4,  5,  6, // Back face
+    4,  6,  7, // Back face
+
+    8,  9,  10, // Right face
+    8,  10, 11, // Right face
+
+    12, 13, 14, // Left face
+    12, 14, 15, // Left face
+
+    16, 17, 18, // Top face
+    16, 18, 19, // Top face
+
+    20, 21, 22, // Bottom face
+    20, 22, 23  // Bottom face
 };
 
 int main() {
